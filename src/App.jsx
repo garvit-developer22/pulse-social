00c53a7e00b47312173a0439f3449d26e02d865d
garvit-developer22 <garvitminivlogs27@gmail.com>
2026-08-27 15:15:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,3 +14,36 @@ import Messages from './Messages'
 import Notifications from './Notifications'
 import Explore from './Explore'
 import Reels from './Reels'
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <ToastProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/auth" element={<Auth />} />
+            <Route
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route path="/" element={<Home />} />
+              <Route path="/create" element={<Create />} />
+              <Route path="/create-story" element={<CreateStory />} />
+              <Route path="/explore" element={<Explore />} />
+              <Route path="/reels" element={<Reels />} />
+              <Route path="/messages" element={<Messages />} />
+              <Route path="/notifications" element={<Notifications />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/user/:uid" element={<UserProfile />} />
+              <Route path="/settings" element={<Settings />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </ToastProvider>
+    </AuthProvider>
+  )
+            }
