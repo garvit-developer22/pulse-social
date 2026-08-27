23de18e0480b1ea6b25b11044338f42f245cf469
garvit-developer22 <garvitminivlogs27@gmail.com>
2026-08-27 19:30:28 +0530

    Update ProtectedRoute.jsx

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,6 +1,5 @@
 import { Navigate } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext'
-
+import { useAuth } from './AuthContext'
 export default function ProtectedRoute({ children }) {
   const { user, loading, configured } = useAuth()
   if (loading) {
